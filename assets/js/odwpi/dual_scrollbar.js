750471cfaa145d49aaf4da105f03d6b5c0d5b048
Danny Guzman <guzmanjd86@gmail.com>
2020-04-29 00:24:59 -0700

    Added WPCS, gulp tasks, Bootstrap 4 Cleaned up codebase

---
+++ assets/js/odwpi/dual_scrollbar.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $(".wmd-view-topscroll").scroll(function(){
+        $(".wmd-view")
+            .scrollLeft($(".wmd-view-topscroll").scrollLeft());
+    });
+    $(".wmd-view").scroll(function(){
+        $(".wmd-view-topscroll")
+            .scrollLeft($(".wmd-view").scrollLeft());
+    });
+});
---
+++ partials/panels/github.php
@@ -0,0 +1,21 @@
+<!-- gitHubTab Column -->
+<div id="github" class="col collapse mt-3 pt-1" data-parent="#odwpi-dev-settings">
+    <div class="mb-2">
+        <strong>User/Organization Name</strong>
+        <input type="text" class="form-control" name="git_user" />
+        <strong>Repository</strong>
+        <input type="text" class="form-control" name="git_repo" />
+    </div>
+    <div class="form-group mb-2 git-private-group hidden">
+        <strong>View Repositories</strong>
+        <span><input type="radio" class="form-control" name="git_view" value="" checked>Info</span>
+        <span><input type="radio" class="form-control" name="git_view" value="issues">Issues</span>
+        <span><input type="radio" class="form-control" name="git_view" value="releases">Releases</span>
+        <strong class="d-block">Is Private Repository? <input type="checkbox" name="git_private_repo" /></strong>
+        <div class="hidden">
+            <strong>Access Token</strong>
+            <input type="text" class="form-control" name="git_token" />
+        </div>
+    </div>
+    <button id="odwpi_dev_git_api" class="btn btn-primary">Test API</button>
+</div>
