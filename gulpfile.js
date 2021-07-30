f51e34ecc8dd464d2e67004469cdf463fd578de7
Danny Guzman <guzmanjd86@gmail.com>
2021-07-30 11:43:29 -0700

    Bumped npm packages Added codemirror css/js

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,7 @@
 /**
  * Gulpfile.
  *
- * Gulp with WordPress.
- *
- * Implements:
- *      1. Live reloads browser with BrowserSync.
- *      2. CSS: Sass to CSS conversion, error catching, Autoprefixing, Sourcemaps,
- *         CSS minification, and Merge Media Queries.
- *      3. JS: Concatenates & uglifies Vendor and Custom JS files.
- *      4. Images: Minifies PNG, JPEG, GIF and SVG images.
- *      5. Watches files for changes in CSS or JS.
- *      6. Watches files for changes in PHP.
- *      7. Corrects the line endings.
- *      8. InjectCSS instead of browser page reload.
- *      9. Generates .pot file for i18n and l10n.
- *
+ * This Gulpfile is a modified version of WPGulp.
  * @tutorial https://github.com/ahmadawais/WPGulp
  * @author Ahmad Awais <https://twitter.com/MrAhmadAwais/>
  */
@@ -32,188 +19,137 @@ const config = require( './wpgulp.config.js' );
  * Load gulp plugins and passing them semantic names.
  */
 const gulp = require( 'gulp' ); // Gulp of-course.
-const parameterized = require('gulp-parameterized');
+
+// Monitoring related plugins.
+const watch = require('gulp-watch');
 
 // CSS related plugins.
-const sass = require( 'gulp-sass' ); // Gulp plugin for Sass compilation.
+const sass = require( 'gulp-sass' )(require('node-sass')); // Gulp plugin for Sass compilation.
 
 // JS related plugins.
-const uglify = require('gulp-uglify'); // Minifies JS files.
+const uglify = require('gulp-uglify-es').default; // Minifies JS files.
 
 // HTML related plugins
 const htmlbeautify = require('gulp-html-beautify'); // Beautify HTML/PHP files
 
-// Image related plugins.
-const imagemin = require( 'gulp-imagemin' ); // Minify PNG, JPEG, GIF and SVG images with imagemin.
-
 // Utility related plugins.
 const concat = require( 'gulp-concat' ); // Concatenates files.
 const lineec = require( 'gulp-line-ending-corrector' ); // Consistent Line Endings for non UNIX systems. Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings).
-const notify = require( 'gulp-notify' ); // Sends message notification to you.
-
 const fs = require('fs'); // File System
+const del = require('del'); // Delete plugin
+var path=require('path');
+
+var argv = require('yargs').argv;
+var log = require('fancy-log');
+var tap = require('gulp-tap');
+
+gulp.task('monitor', function(){
+	watch(['assets/**/*'], function(cb) {
+		buildAllAssets();
+	});
+});
 
 /*
 	CAWeb NetAdmin Admin Styles
 */
-gulp.task('admin-css', parameterized( async function (_) {
-	var noFlags = undefined === _.params.length || _.params.all;
+gulp.task('admin-css', async function () {
+	del( ['css/admin*.css'] );
 
-	if ( _.params.prod ) {
+	if ( argv.prod ) {
 		buildAdminStyles(true);
 	}
 
-	if ( _.params.dev ) {
+	if ( argv.dev ) {
 		buildAdminStyles(false);
 	}
 
-	if( noFlags ){
+	if( buildAll(argv) ){
 		buildAdminStyles(true);
 		buildAdminStyles(false);
 	}
-}));
-
-/*
-	CAWeb NetAdmin FrontEnd Styles
-*/
-gulp.task('frontend-css', parameterized( async function (_) {
-	var noFlags = undefined === _.params.length || _.params.all;
-
-	if ( _.params.prod ) {
-		buildFrontEndStyles(SVGComponentTransferFunctionElement);
-	}
-
-	if ( _.params.dev ) {
-		buildFrontEndStyles(false);
-	}
-
-	if( noFlags ){
-		buildFrontEndStyles(false);
-		buildFrontEndStyles(true);
-	}
-
-}));
+});
 
 /*
 	CAWeb NetAdmin Admin JavaScript
 */
-gulp.task('admin-js', parameterized( async function (_) {
-	var noFlags = undefined === _.params.length || _.params.all;
+gulp.task('admin-js', async function () {
+	del( ['js/admin*.js'] );
 
-	if ( _.params.prod ) {
+	if ( argv.prod ) {
 		buildAdminJS(true);
 	}
 
-	if ( _.params.dev ) {
+	if ( argv.dev ) {
 		buildAdminJS(false);
 	}
 
-	if( noFlags ){
+	if( buildAll(argv) ){
 		buildAdminJS(true);
 		buildAdminJS(false);
 	}
-}));
+});
 
-/*
-	CAWeb NetAdmin FrontEnd JavaScript
-*/
-gulp.task('frontend-js', parameterized( async function (_) {
-	var noFlags = undefined === _.params.length || _.params.all;
-
-	if ( _.params.prod ) {
-		buildFrontEndJS(true);
-	}
-
-	if ( _.params.dev ) {
-		buildFrontEndJS(false);
-	}
-
-	if( noFlags ){
-		buildFrontEndJS(true);
-		buildFrontEndJS(false);
-	}
-}));
-
-
-
-gulp.task('beautify', parameterized(async function(_) {
+gulp.task('beautify', async function() {
 	var options = {indentSize: 2};
-	var noFlags = ! Object.getOwnPropertyNames(_.params).length || undefined === _.params.file;
-	var src = ['*.php', '*.html'];
+	var src = ['**/*.php'];
 
-	if( ! noFlags ){
-		src = _.params.file;
+	if( argv.hasOwnProperty('file') ){
+		src = argv.file;
 	}
 	
 	gulp.src(src, {base: './'})
 	  .pipe(htmlbeautify(options))
 	  .pipe(gulp.dest('./'));
 	
-}));
+});
 
 /*
 	CAWeb Build All CSS/JS and Beautify
 */
-gulp.task('build', parameterized(async function(_){
-	var noFlags = ! Object.getOwnPropertyNames(_.params).length || undefined !== _.params.all;
-	var versionNum = undefined !== _.params.ver ? _.params.ver : false;
+gulp.task('build', async function(){
+	buildAllAssets();
+});
 
-	if ( _.params.prod ) {
+
+// Gulp Task Functions
+async function buildAllAssets(){
+	var version = undefined !== argv.ver ? [ argv.ver ] : config.availableVers;
+
+	del( ['js/*.js', 'css/*.css'] );
+	
+	if ( argv.prod ) {
 		// Build Admin Styles
 		buildAdminStyles(true);
-
-		// Build Version Styles
-		buildFrontEndStyles(true);
-
+		
 		// Build Admin JS
 		buildAdminJS(true);
-
-		// Build Frontend JS
-		buildFrontEndJS(true);
-
 	}
 
-	if ( _.params.dev ) {
+	if ( argv.dev ) {
 		// Build Admin Styles
 		buildAdminStyles(false);
-
-		// Build Front End Styles
-		buildFrontEndStyles(false);
-
+		
 		// Build Admin JS
 		buildAdminJS(false);
-
-		// Build Frontend JS
-		buildFrontEndJS(false);
-
 	}
 
-	if( noFlags ){
+	if( buildAll(argv) ){
 		// Build Admin Styles
 		buildAdminStyles(true);
 		buildAdminStyles(false);
 		
-		// Build Front End Styles
-		buildFrontEndStyles(true);
-		buildFrontEndStyles(false);
-
 		// Build Admin JS
 		buildAdminJS(true);
 		buildAdminJS(false);
-
-		// Build Frontend JS
-		buildFrontEndJS(true);
-		buildFrontEndJS(false);
-
 	}
 
-}));
+}
 
-
-// Gulp Task Functions
 async function buildAdminStyles( min = false){
 	var buildOutputStyle = min ? 'compressed' : 'expanded';
 	var minified = min ? '.min' : '';
+	var t = minified ? ' Minified ] ' : ' ] ';
+	t = '[ ✅ ODWPI Admin Styles' + t;
 
 	if( ! config.adminCSS.length )
 		return;
@@ -226,30 +162,16 @@ async function buildAdminStyles( min = false){
 		)
 		.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
 		.pipe(concat('admin' + minified + '.css')) // compiled file
-		.pipe( notify({ title: '✅  CAWeb Admin Styles', message: '<%= file.relative %> was created successfully.', onLast: true }) )
+		.pipe( tap(function(file) {
+			log(t + path.basename( file.path ) + ' was created successfully.' ); 
+		}) )
 		.pipe(gulp.dest('./css/'));
-}
-
-async function buildFrontEndStyles( min = false ){
-	var buildOutputStyle = min ? 'compressed' : 'expanded';
-	var minified = min ? '.min' : '';
-	
-	if( ! config.frontendCSS.length )
-		return;
-
-		return gulp.src(config.frontendCSS, {allowEmpty: true} )
-			.pipe(
-				sass({
-					outputStyle: buildOutputStyle,
-				})
-			)
-			.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
-			.pipe(concat('admin' + minified + '.css' )) // compiled file
-			.pipe(gulp.dest('./css/'));
 }
 
 async function buildAdminJS( min = false){
 	var minified = min ? '.min' : '';
+	var t = minified ? ' Minified ] ' : ' ] ';
+	t = '[ ✅ ODWPI Admin Scripts' + t;
 
 	if( ! config.adminJS.length )
 		return;
@@ -257,7 +179,9 @@ async function buildAdminJS( min = false){
 	let js = gulp.src(config.adminJS, { allowEmpty: true } )
 		.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
 		.pipe(concat('admin' + minified + '.js')) // compiled file
-		.pipe( notify({ title: '✅  CAWeb Admin JavaScript', message: '<%= file.relative %> was created successfully.', onLast: true }) )
+		.pipe( tap(function(file) {
+			log(t + path.basename( file.path ) + ' was created successfully.' ); 
+		}) )
 
 
 	if( min ){
@@ -267,28 +191,9 @@ async function buildAdminJS( min = false){
 	return js.pipe(gulp.dest('./js/'));
 }
 
-async function buildFrontEndJS( min = false){
-	var minified = min ? '.min' : '';
+function buildAll( params = {} ){
+	var b = params.hasOwnProperty('all') || ! Object.keys(params).length;
+	var p = params.hasOwnProperty('dev') || params.hasOwnProperty('prod');
 
-	if( ! config.frontendJS.length )
-		return;
-
-	let js = gulp.src(config.frontendJS, { allowEmpty: true } )
-		.pipe( lineec() ) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('frontend' + minified + '.js')) // compiled file
-		.pipe( notify({ title: '✅  CAWeb Front End JavaScript', message: '<%= file.relative %> was created successfully.', onLast: true }) );
-
-	if( min ){
-		js = js.pipe(uglify());
-	}
-
-	return js.pipe(gulp.dest('./js/'));
+	return  b || ! p ;
 }
-
-//
-// DEV (Development Output)
-//
-gulp.task('dev', parameterized.series('frontend-css --dev', 'admin-css --dev', 'admin-js --dev', 'frontend-js --dev') );
-
-// PROD (Minified Output)
-gulp.task('prod', parameterized.series('frontend-css --prod', 'admin-css --prod', 'admin-js --prod', 'frontend-js --prod') );
